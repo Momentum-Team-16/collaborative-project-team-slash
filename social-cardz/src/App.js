4e9cdb5b63139068ddbaa42edfca1fb9d75f2e10
Lucian Lodico <llodico@mac.com>
2023-01-31 16:06:10 -0500

    lil mock data

--- social-cardz/src/App.js
+++ social-cardz/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react'; 
 
-function App() {
+const App = ({ card }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div>
+      <h1>Social Cardz</h1>
+      {card.map((card) => (
+        <div className="card-cover">
+          <h2>{card.title}</h2>
+          <h3>{card.author}</h3>
+          <p>{card.shortDescription}</p>
+        </div> 
+      ))}
     </div>
   );
 }
