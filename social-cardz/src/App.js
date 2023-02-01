a9ee9320e5426145a34d9aad7bf702380d0f1d97
Lucian Lodico <llodico@mac.com>
2023-01-31 19:38:29 -0500

    mock cards displaying

--- social-cardz/src/App.js
+++ social-cardz/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
-import React from 'react'; 
+import React from 'react';
+import moment from 'moment'; 
 
 const App = ({ card }) => {
   return (
-    <div>
-      <h1>Social Cardz</h1>
+  <>
+  <p className="time">Date: {moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
+    <h1>Social Cardz</h1>
+    <div className="bob">
       {card.map((card) => (
         <div className="card-cover">
           <h2>{card.title}</h2>
           <h3>{card.author}</h3>
           <p>{card.shortDescription}</p>
+          <br/>  
+          <p>{card.date}</p>
         </div> 
       ))}
     </div>
+  </> 
   );
 }
 
