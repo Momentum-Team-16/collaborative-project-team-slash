ccf7c80eb8f020a288e57e3ae5364eecbeb359d1
Cameron Poulton <Crp2108@hotmail.com>
2023-02-09 14:22:49 -0500

    added-unfollow

--- social-cardz/src/components/Unfollow.js
+++ social-cardz/src/components/Unfollow.js
@@ -1,8 +1,15 @@
-import { requestUnfollowUser } from './Requests'; 
+import { requestUnfollowUser } from "./Requests"
 
-export const Unfollow = () => {
+export const Unfollow = ({ token, ownerId}) => {
+
+    const unfollowUser = () => {
+
+        requestUnfollowUser(token, ownerId) 
+            .then((res) => res.data === '' && (window.location.reload())) /* change this (window.location.reload()) */ 
+            console.log("unfollowed")
+    }
 
     return (
-        <button>Dump Leafbag</button>
+        <button className="erasebutt" onClick={unfollowUser}>Dump Leafbag</button>
     )
 }
